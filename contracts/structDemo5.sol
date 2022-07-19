17b107f9666072f4ac6f71431f7da446e501ca9d
hss <haoshanshan@tianrukj.com>
2022-07-19 10:05:54 +0800

    add practice examples

--- contracts/structDemo5.sol
+++ contracts/structDemo5.sol
@@ -19,14 +19,14 @@ contract structDemo {
     // }
 
     //指针传递
-    function test(student storage s) internal {
-        student storage meimei = s;
+    function test(student memory s) internal {
+        student memory meimei = s;
         meimei.name = "cangjingkong";
     }
 
     function call() public returns(string memory, uint) {
         //student storage lily = student(100, "lily");  编译失败，方法体内须使用memory类型、方法体内使用storage须从方法体外部引用
-        student storage lily = student(100, "lily");
+        student memory lily = student(100, "lily");
         test(lily);
         return (lily.name, lily.grade);
     }
